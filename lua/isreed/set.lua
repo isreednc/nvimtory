17e1b55142caa1dd8b5b8ca4daf88009bca0acb0
isreednc <isaacreednc99@gmail.com>
2025-04-11 17:39:34 -0400

    LSP, Harpoon

--- lua/isreed/set.lua
+++ lua/isreed/set.lua
@@ -21,4 +21,69 @@ vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
 
+vim.api.nvim_create_autocmd('TermOpen', {
+    group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
+    callback = function()
+        vim.opt.number = false
+        vim.opt.relativenumber = false
+    end,
+})
+
+vim.keymap.set("n", "<leader>st", function()
+    vim.cmd.vnew()
+    vim.cmd.term()
+    vim.cmd.wincmd("J")
+    vim.api.nvim_win_set_height(0, 5)
+end)
+
+
+local function is_terminal_window()
+  local bufnr = vim.api.nvim_get_current_buf()
+  return vim.bo[bufnr].buftype == 'terminal'
+end
+
+local function toggle_terminal()
+  if is_terminal_window() then
+    -- Close the current terminal window
+    vim.api.nvim_win_close(0, true)
+    return
+  end
+
+  -- Search for an existing terminal window
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    local buf = vim.api.nvim_win_get_buf(win)
+    if vim.bo[buf].buftype == 'terminal' then
+      vim.api.nvim_set_current_win(win)
+      return
+    end
+  end
+
+  -- Search for an existing terminal buffer (not currently shown)
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.bo[buf].buftype == 'terminal' then
+      vim.cmd.vsplit() -- open a vertical split
+      vim.api.nvim_set_current_buf(buf)
+      vim.cmd.wincmd("J")
+      vim.api.nvim_win_set_height(0, 5)
+      return
+    end
+  end
+
+  -- If no terminal buffer exists, create a new one
+  vim.cmd.vsplit()
+  vim.cmd.term()
+  vim.cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0, 5)
+end
+
+vim.keymap.set('n', '<leader>tt', toggle_terminal, { desc = "Toggle Terminal" })
+
+
+-- Telescope nvim config from anywhere --
+vim.keymap.set("n", "<leader>fc", function()
+  require("telescope.builtin").find_files({
+    prompt_title = "Neovim Config",
+    cwd = vim.fn.stdpath("config"),
+  })
+end, { desc = "Telescope find in Neovim config" })
 
--- lua/plugins/spec.lua
+++ lua/plugins/spec.lua
@@ -21,5 +21,73 @@ return {
 	      })
 	    end,
 	},
+    -- mason.nvim for managing LSP binaries
+    {
+      "williamboman/mason.nvim",
+      build = ":MasonUpdate",
+      config = function()
+        require("mason").setup()
+      end,
+    },
+
+    -- mason-lspconfig to bridge mason and nvim-lspconfig
+    {
+      "williamboman/mason-lspconfig.nvim",
+      dependencies = {
+        "williamboman/mason.nvim",
+        "neovim/nvim-lspconfig",
+      },
+      config = function()
+        require("mason-lspconfig").setup {
+          ensure_installed = {
+            "pyright",
+            "gopls",
+            "ts_ls",
+            "clangd",
+          },
+        }
+
+        local lspconfig = require("lspconfig")
+
+        -- Set up each server
+        local servers = {
+          pyright = {},
+          gopls = {},
+          tsserver = {},
+          clangd = {},
+        }
+
+        for name, config in pairs(servers) do
+          lspconfig[name].setup(config)
+        end
+      end,
+    },
+
+    -- Core LSP support
+    {
+      "neovim/nvim-lspconfig",
+      event = { "BufReadPre", "BufNewFile" },
+    },
+
+    -- Harpoon
+    {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            local harpoon = require('harpoon')
+            harpoon:setup()
+
+            vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end)
+            vim.keymap.set("n", "<leader>hd", function() harpoon:list():remove() end)
+            vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+            vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+            vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
+            vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
+            vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
+        end,   
+    }
 
 }
+
